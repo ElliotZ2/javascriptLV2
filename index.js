209a7ad806b218ca87a260b5b0b25436ab5fbab9
ElliotZ2 <elliotz2@nycstudents.net>
2022-12-09 13:42:32 -0500

    bb

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var oneSelected = false;
 var correctTiles = [];
 var selectedTiles = [];
 var allTiles = [];
+var attempts = 0;
 
 function initialize() {
     populateGrid();
@@ -21,12 +22,11 @@ function populateGrid() {
     for(let i = 0; i < 8; i++) {
         let tile1 = document.createElement("div");
         let tile2 = document.createElement("div");
-        tile1.style.backgroundColor = "#1AFF1A";
-        tile2.style.backgroundColor = "#1AFF1A";
         tile1.setAttribute("id", ""+i+"A");
         tile2.setAttribute("id", ""+i+"B");
         tile1.classList.add("tile");
         tile2.classList.add("tile");
+        /*show innerhtml for testing*/
         tile1.innerHTML = i;
         tile2.innerHTML = i;
         tile1.addEventListener('click', function(){
@@ -68,10 +68,11 @@ function tileClick(tile) {
         }
         else{//guessed incorrectly
             console.log("Guessed incorrectly.");
-
         }
         selectedTiles=[];
         oneSelected=false;
+        unrevealTiles();
+        attempts++;
     }
     else{
         oneSelected=true;
@@ -86,5 +87,15 @@ function checkForWin() {
 }
 
 function revealTile(tile) {
-    
+    let idNum=tile.id;
+    idNum=idNum.charAt(0);
+    tile1.style.backgroundImage="images/"+idNum+".png";
+}
+
+function unrevealTiles() {
+    for(let i = 0; i < allTiles.length; i++) {
+        if(!correctTiles.includes(allTiles[i])) {
+            allTiles[i].style.backgroundImage='none';
+        }
+    }
 }
